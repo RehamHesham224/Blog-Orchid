d90840a5d693a685a84cba769110162513eada54
Reham Hesham <63992238+RehamHesham224@users.noreply.github.com>
2022-03-19 05:48:27 +0200

    complete Orchied installation

--- database/migrations/2015_10_19_214424_create_orchid_roles_table.php
+++ database/migrations/2015_10_19_214424_create_orchid_roles_table.php
@@ -4,28 +4,27 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateOrchidRolesTable extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up()
     {
-        Schema::create('user_role', function (Blueprint $table) {
-            $table->id();
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug')->unique();
+            $table->string('name');
+            $table->jsonb('permissions')->nullable();
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down()
     {
-        Schema::dropIfExists('user_role');
+        Schema::dropIfExists('roles');
     }
-};
+}
